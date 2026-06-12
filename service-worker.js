c926903322ee2c7929b84be49dfb6c506c64250b
Finley Patterson <finley_patterson@icloud.com>
2026-06-12 14:06:37 +0000

    Update manifest.json and service worker for improved caching and file paths

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
 // This file runs in the background and makes your app work offline
  
 // Give your cache a name - change this if you update your app files
-const CACHE_NAME = 'door-detective-cache-v2';
+const CACHE_NAME = 'door-detective-cache-v3';
  
 // List every file your app needs to work
 const FILES_TO_CACHE = [
-  './',
   './index.html',
   './style.css',
   './script.js',
@@ -54,6 +53,7 @@ const FILES_TO_CACHE = [
 // INSTALL - runs once when the app is first visited
 // Downloads and saves all your files into the cache
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -64,6 +64,7 @@ self.addEventListener('install', event => {
 // ACTIVATE - runs when a new version of this file is detected
 // Deletes old caches so users get your updated files
 self.addEventListener('activate', event => {
+  self.clients.claim();
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
